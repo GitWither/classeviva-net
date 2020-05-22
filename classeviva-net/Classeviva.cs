7195e3d3926d519a1766f2470c340c9b72706d6c
Wither <wither.tweets@gmail.com>
2020-05-22 21:12:27 +0200

    Started writing Grade getter

--- classeviva-net/Classeviva.cs
+++ classeviva-net/Classeviva.cs
@@ -6,6 +6,7 @@ using HtmlAgilityPack;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using ClassevivaNet.Internal;
+using System.Linq;
 
 namespace ClassevivaNet
 {
@@ -65,7 +66,6 @@ namespace ClassevivaNet
             HttpResponseMessage msg = await client.PostAsync("https://web.spaggiari.eu/auth-p7/app/default/AuthApi4.php?a=aLoginPwd", content);
             string[] cookies = (string[])msg.Headers.GetValues("Set-Cookie");
             ResponseBody body = JsonConvert.DeserializeObject<ResponseBody>(await msg.Content.ReadAsStringAsync());
-            Console.WriteLine(await msg.Content.ReadAsStringAsync());
 
             if (cookies != null)
             {
@@ -88,7 +88,7 @@ namespace ClassevivaNet
         /// Gets the student's school name
         /// </summary>
         /// <returns>A string containing the school name</returns>
-        public async Task<string> GetSchool()
+        public async Task<string> GetSchoolAsync()
         {
 
             HttpResponseMessage msg = await http.GetAsync("https://web.spaggiari.eu/home/app/default/menu_webinfoschool_genitori.php");
@@ -109,7 +109,7 @@ namespace ClassevivaNet
         /// Gets the student's full name WARNING: This method is slower than the Name and the Surname properties, but is included for consisency
         /// </summary>
         /// <returns>A string containing the students full name</returns>
-        public async Task<string> GetFullName()
+        public async Task<string> GetFullNameAsync()
         {
 
             HttpResponseMessage msg = await http.GetAsync("https://web.spaggiari.eu/home/app/default/menu_webinfoschool_genitori.php");
@@ -126,13 +126,26 @@ namespace ClassevivaNet
             }
         }
 
+        public async Task GetGrades()
+        {
+            HttpResponseMessage msg = await http.GetAsync("https://web.spaggiari.eu/cvv/app/default/genitori_note.php?filtro=tutto");
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(await msg.Content.ReadAsStringAsync());
+            HtmlNode[] dataTableNodes = doc.GetElementbyId("data_table").ChildNodes.ToArray();
+            Console.WriteLine(dataTableNodes.Length);
+            for (int i = 10; i < dataTableNodes.Length; i++)
+            {
+                Console.WriteLine(dataTableNodes[i].GetClasses()?.ToArray()[0]);
+            }
+        }
+
         /// <summary>
         /// Gets all the homework assigned within the DateTime range given
         /// </summary>
         /// <param name="startDate">Start date</param>
         /// <param name="endDate">End date</param>
         /// <returns>An array of homework objects</returns>
-        public async Task<Homework[]> GetHomework(DateTime startDate, DateTime endDate)
+        public async Task<Homework[]> GetHomeworkAsync(DateTime startDate, DateTime endDate)
         {
             HttpResponseMessage msg = await http.GetAsync(
                 $"https://web.spaggiari.eu/fml/app/default/agenda_studenti.php?ope=get_events&classe_id=&gruppo_id=&start=" +
